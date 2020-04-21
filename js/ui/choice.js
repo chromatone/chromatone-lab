aa41a6c59d9d602a937b2b9abab4f09a0fd6e0d4
DeFUCC <fond@fondfrukt.ru>
2020-04-21 15:15:34 +0300

    pretty triggers

--- js/ui/choice.js
+++ js/ui/choice.js
@@ -6,7 +6,7 @@ export const choice = {
       <span  class="title"><slot></slot></span>
       <button
         v-for="(option, key) in options"
-        :class="{active:value==key}" @click="$emit('input',key)" :key="key">
+        :class="{active:value==key, inactive:value!=key}" @click="$emit('input',key)" :key="key">
         {{option}}
       </button>
     </div>
